3b7bc5a98296ddfbd89cdc82852b00cd44fe97e8
Muzxa <muzza042004@gmail.com>
2026-03-13 00:18:50 +0500

    feat(adapter): create MovieListAdapter for RecyclerView movie list
    - Binds movie poster, title, genre/runtime to each card
    - Opens YouTube trailer (with YouTube app fallback) on Trailer button click
    - Book Seats click handler stubbed for future implementation

---
+++ app/src/main/java/com/example/cinefast/MovieListAdapter.java
@@ -0,0 +1,61 @@
+package com.example.cinefast;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.utils.widget.MockView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListViewHolder> {
+
+    Context context;
+    ArrayList<Movie> movies;
+
+    public MovieListAdapter(Context context, ArrayList<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public MovieListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MovieListViewHolder(LayoutInflater.from(context).inflate(R.layout.movie_card_view, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieListViewHolder holder, int position) {
+
+        Movie movie = movies.get(position);
+        holder.tvTitle.setText(movie.getName());
+        holder.tvGenreRuntime.setText(movie.getGenre() + " | " + movie.getRuntime());
+        holder.ivPoster.setImageResource(movie.getPosterId());
+
+        holder.btnTrailer.setOnClickListener(v -> {
+
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(movie.getTrailerURL()));
+            i.setPackage("com.google.android.youtube");
+            try{
+                context.startActivity(i);
+            } catch (ActivityNotFoundException e) {
+                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(movie.getTrailerURL())));
+            }
+        });
+
+        holder.btnBook.setOnClickListener(v -> {
+            //TODO: IMPLEMENT
+        });
+    }
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+}
