4ea12db34aa206efa42e522a61e8358e84bf23b8
Muzxa <muzza042004@gmail.com>
2026-03-13 00:19:27 +0500

    feat(adapter): add MovieListViewPagerAdapter for tab-based fragment navigation
    - Extends FragmentStateAdapter with 2 pages
    - Returns FragmentNowShowing for position 0, FragmentComingSoon for position 1

---
+++ app/src/main/java/com/example/cinefast/MovieListViewPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.cinefast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MovieListViewPagerAdapter extends FragmentStateAdapter {
+
+    final private int FRAGMENT_COUNT = 2;
+
+    public MovieListViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position)
+        {
+            case 1:
+                return new FragmentComingSoon();
+            default:
+                return new FragmentNowShowing();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return FRAGMENT_COUNT;
+    }
+}
