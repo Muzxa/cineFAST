dc80f1369abe2bda867b3c9a412d55c80d4acd5f
Muzxa <muzza042004@gmail.com>
2026-04-26 21:50:14 +0500

    refactor(coming_soon): consume movie list from parent instead of hardcoding data
    - add factory method FragmentComingSoon.newInstance() accepting movies list
    - extract movies from Bundle arguments in onCreate()
    - remove hardcoded movie creation and date parsing from onViewCreated()
    - allow adapter to populate recycler from parent-provided movie data
    - simplify fragment to presentation-only responsibility
    - align with now_showing fragment data-driven pattern

--- app/src/main/java/com/example/cinefast/FragmentComingSoon.java
+++ app/src/main/java/com/example/cinefast/FragmentComingSoon.java
@@ -12,15 +12,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class FragmentComingSoon extends Fragment {
+    private static final String ARG_MOVIES = "arg_movies";
+
     RecyclerView comingSoonRecyclerView;
     ArrayList<Movie> movies;
+
+    public static FragmentComingSoon newInstance(ArrayList<Movie> movies) {
+        FragmentComingSoon fragment = new FragmentComingSoon();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(ARG_MOVIES, movies);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        movies = new ArrayList<>();
+
+        if (getArguments() != null) {
+            ArrayList<Movie> passedMovies = getArguments().getParcelableArrayList(ARG_MOVIES);
+            if (passedMovies != null) {
+                movies.addAll(passedMovies);
+            }
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,23 +47,12 @@ public class FragmentComingSoon extends Fragment {
         View view = inflater.inflate(R.layout.fragment_coming_soon, container, false);
 
         comingSoonRecyclerView = view.findViewById(R.id.coming_soon_recycler_view);
-        movies = new ArrayList<>();
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-
-        try {
-            movies.add(new Movie("The Dark Knight", "152 mins", "Action", 1, "20:15", R.drawable.the_dark_knight, "https://www.youtube.com/watch?v=EXeTwQWrcwY", format.parse("2026-12-01")));
-            movies.add(new Movie("12 Angry Men", "96 mins", "Crime", 12, "15:30", R.drawable.twelve_angry_men, "https://www.youtube.com/watch?v=TEN-2uTi2c0", format.parse("2026-12-02")));
-            movies.add(new Movie("Saving Private Ryan", "169 mins", "War", 5, "13:00", R.drawable.saving_private_ryan, "https://www.youtube.com/watch?v=9CiW_DgxCnQ", format.parse("2026-12-03")));
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
 
         comingSoonRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
         comingSoonRecyclerView.setAdapter(new MovieListAdapter(requireContext(), movies));
